d3d746d1583d3853c32f44eda0add9841292394e
Jim NTARE <jntare@bk.rw>
2020-10-08 19:46:15 +0200

    ch(setup coveralls):
    create coveralls.yml file
    install coveralls dependency
    
    [ Finishes #175138569 ]

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# stockloop-backend
+# Stockloop-Backend API
 Inventory Management System backend system
+
+[![Build Status](https://travis-ci.org/Ntare22/stockloop-backend.svg?branch=develop)](https://travis-ci.org/Ntare22/stockloop-backend)
+[![Coverage Status](https://coveralls.io/repos/github/Ntare22/stockloop-backend/badge.svg?branch=develop)](https://coveralls.io/github/Ntare22/stockloop-backend?branch=develop)
--- src/env.js
+++ src/env.js
@@ -1,17 +1,10 @@
 import dotenv from 'dotenv-extended';
 import { resolve } from 'path';
 
-const envName = (process.env.NODE_ENV || '').toLowerCase();
-const path = resolve(__dirname, `../.env.${envName}`)
-const isTest = envName === 'test';
-const isDevelopment = envName === 'development';
-
 dotenv.load({
     silent: true,
-    path,
     defaults: resolve(__dirname, '../.env'),
     schema: resolve(__dirname, '../.env.sample'),
-    errorOnMissing: !isTest,
     errorOnExtra: false,
     errorOnRegex: false,
     includeProcessEnv: true,
@@ -20,10 +13,7 @@ dotenv.load({
 
 export default {
     ...process.env,
-    PORT: isDevelopment || isTest ? 4000 : process.env.PORT,
-    NODE_ENV: process.env.NODE_ENV || 'production',
-    isTest,
-    isDevelopment,
+    PORT: process.env.PORT,
     BACKEND_URL: process.env.BACKEND_URL,
     TEST_DB: process.env.TEST_DB,
     DEV_DB: process.env.DEV_DB,
