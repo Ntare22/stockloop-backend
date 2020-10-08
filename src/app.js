225897c0ba16dfc534fde228f4e169cc6d3eee0b
Jim NTARE <jntare@bk.rw>
2020-10-08 18:55:39 +0200

    ch(setup travis ci):
    setup travis.yml file in the root folder of the project
    
    [ Finishes #175109784 ]

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 import express from 'express';
-// import swagger from './helpers/swagger.config';
 import HomeRouter from './modules/home';
 
 const App = express.Router();
 
-// App.use('/api-docs', swagger);
 App.use(HomeRouter);
 
 export default App;
