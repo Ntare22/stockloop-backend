7d7f3eb0c98277bee0a9a4138ef2647635003af3
Jim NTARE <jntare@bk.rw>
2020-10-24 15:59:32 +0200

    ch(setup heroku):
    add procfile to project
    
    [ Finishes #175189571 ]

--- src/env.js
+++ src/env.js
@@ -18,7 +18,6 @@ export default {
     BACKEND_URL: process.env.BACKEND_URL,
     TEST_DB: process.env.TEST_DB,
     DEV_DB: process.env.DEV_DB,
-    PROD_DB: process.env.PROD_DB,
     BASE_URL: process.env.BASE_URL,
     SALT: process.env.SALT,
     JWTKEY: process.env.JWTPRIVATEKEY
